4238adf35fd6915c2e4e2ada6eedd52d529e80b2
Apekshaanand <apekshaanand.aa@gmail.com>
2019-09-09 10:16:25 +0530

    [age update]

---
+++ new age.py
@@ -0,0 +1,11 @@
+age = int(input("enter the age"))
+if(2<=age<=5):
+    print("kid student")
+elif(6<=age<=13):
+    print("school student")
+elif(14<=age<=16):
+     print("high school")
+else:
+    print("go to college")
+
+
---
+++ pract.py
@@ -0,0 +1,5 @@
+age=int(input("Enter the input"))
+if (age>18):
+    print ("it is true")
+else:
+    print ("it is false")
