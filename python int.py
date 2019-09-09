2f140a4796db2a45debdfbe2582e66be24e16a0d
Apekshaanand <apekshaanand.aa@gmail.com>
2019-09-09 10:47:50 +0530

    grade values

---
+++ python int.py
@@ -0,0 +1,11 @@
+score=int(input("Enter the input : "))
+if(25<score<=30):
+    print("A")
+elif(20<score<=25):
+    print("B")
+elif(10<score<=15):
+    print("C")
+elif(5<score<=10):
+    print("D")
+elif(0<score<=5):
+    print("F")
